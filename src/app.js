c8f052e7fb6dc4e1f2e0f9c2cdfbdbb76a41168d
Android Studio <devandroid@ort>
2026-04-20 09:00:41 -0300

    TODOELMALDITOTRABAJOANGIE

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express";
-import {horaActual} from "./time"
+import {horaActual, fechaActual} from "./time.js"
 
+
+console.log("estamos trabajando angi")
 const app = express();
 const PORT = 3000;
 
+
 app.get('/', (req, res) => {
+    res.sendFile("C:/Users/devandroid/Desktop/TP_04_DAI/src/img/mike.png");
+});
+
+app.get('/hora', (req, res) => {
     res.send(horaActual());
 });
 
+app.get('/fecha-completa', (req, res) => {
+    res.send(fechaActual());
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("esto no funciona angi-ene de portriene");
+});
+
 app.listen(PORT, () => {
-    console.log("Servidor corriendo en puerto 3000");
+    console.log("Servidor corriendo en puerto 3000 y siendo escuchado atentamente");
 });
--- src/time.js
+++ src/time.js
@@ -1,10 +1,33 @@
-function horaActual(){
+function horaActual() {
     let ahora = new Date();
 
-    let hora = `${ahora.getHours()}:${ahora.getMinutes()}:${ahora.getSeconds()}`  
+    let hora = `${ahora.getHours()}:${ahora.getMinutes()}:${ahora.getSeconds()}`
 
     return hora
 }
 
+function fechaActual() {
+    let ahora = new Date();
+    const dias = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"]
+    const meses = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    ];
+    ahora = `${dias[ahora.getDay()]} ${ahora.getDate()} de ${meses[ahora.getMonth()]} de ${ahora.getFullYear()}, ${horaActual()}`
 
-export  {horaActual}
+    return ahora
+}
+
+console.log(horaActual())
+
+export { horaActual, fechaActual }
